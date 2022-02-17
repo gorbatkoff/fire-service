01830e47a55a694c94755388ac5d0e2e0da47f42
gorbatkoff <gorbatkovartyom@gmail.com>
2022-02-17 21:05:59 +0300

    Add anchors

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,12 +6,12 @@ export default function Header() {
             <header>
                 <div className="nav">
                     <ul className="list">
-                        <li className="nav-link"><a href="">О Нас</a></li>
-                        <li className="nav-link"><a href="">Программы Обучения</a></li>
-                        <li className="nav-link"><a href="">Галерея</a></li>
-                        <li className="nav-link"><a href="">Документы</a></li>
-                        <li className="nav-link"><a href="">Контакты</a></li>
-                        <li className="nav-link" ><a className='selected' href="">Войти</a></li>
+                        <li className="nav-link"><a href="#about">О Нас</a></li>
+                        <li className="nav-link"><a href="#study-main">Программы Обучения</a></li>
+                        <li className="nav-link"><a href="#slider">Галерея</a></li>
+                        <li className="nav-link"><a href="/documents">Документы</a></li>
+                        <li className="nav-link"><a href="#contacts">Контакты</a></li>
+                        <li className="nav-link" ><a className='selected' href="/login">Войти</a></li>
                     </ul>
                 </div>
             </header>

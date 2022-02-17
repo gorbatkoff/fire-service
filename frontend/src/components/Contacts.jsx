db6f8697b929e74c4d88bfb5406e5bba578d0617
gorbatkoff <gorbatkovartyom@gmail.com>
2022-02-17 21:05:13 +0300

    Change class to className

--- frontend/src/components/Contacts.jsx
+++ frontend/src/components/Contacts.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
+import { YMaps, Map } from 'react-yandex-maps'
 
 export default function Contacts() {
     return (
         <section>
-            <div class="contacts">
-                <div class="communication">
-                    <div class="address">
+            <div className="contacts" id="contacts">
+                <div className="communication">
+                    <div className="address">
                         <div>
                             <h2>Адрес: </h2>ул. Максима Горького, 206, Ростов-на-Дону, Россия,
                         </div>
                         <div>Индекс: 304824</div>
                     </div>
 
-                    <div class="phone">
+                    <div className="phone">
                         <h2>Телефон: </h2>
                         +7 (495) 123-45-67
                         <br />
                         +7 (890) 765 43-21
                     </div>
 
-                    <div class="mails">
+                    <div className="mails">
                         <h2>
                             Электронный Адрес
                         </h2>
@@ -27,10 +28,15 @@ export default function Contacts() {
                     </div>
                 </div>
 
-                <div class="map">
+                <div className="map">
                     <h2>Карта</h2>
 
-                    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A0054a8da1eecbc8bffb5244db332e1fcbc2ea787fe160a0945f52f532dc6b6b8&amp;width=800&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
+                    <YMaps>
+                        <div style={{width: 850}}>
+                            <Map defaultState={{ center: [47.230223, 39.732575], zoom: 17 }} 
+                            style={{width: 850, height: 400}} />
+                        </div>
+                    </YMaps>
                 </div>
             </div>
         </section>

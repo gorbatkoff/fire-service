691b8622dd8e76c12ac8957bfeaaca9bfe7fcec9
gorbatkoff <gorbatkovartyom@gmail.com>
2022-02-18 00:36:56 +0300

    Add routing

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import {Link} from 'react-router-dom';
+
 export default function Header() {
     return (
         <div>
@@ -9,9 +11,9 @@ export default function Header() {
                         <li className="nav-link"><a href="#about">О Нас</a></li>
                         <li className="nav-link"><a href="#study-main">Программы Обучения</a></li>
                         <li className="nav-link"><a href="#slider">Галерея</a></li>
-                        <li className="nav-link"><a href="/documents">Документы</a></li>
+                        <li className="nav-link"><Link to="/documents">Документы</Link></li>
                         <li className="nav-link"><a href="#contacts">Контакты</a></li>
-                        <li className="nav-link" ><a className='selected' href="/login">Войти</a></li>
+                        <li className="nav-link" ><Link id="login" to="/login">Войти</Link></li>
                     </ul>
                 </div>
             </header>

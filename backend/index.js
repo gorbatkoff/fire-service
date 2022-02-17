f9e13c76f9f0e590a20ea7c9f8eb091ed1999d6b
gorbatkoff <gorbatkovartyom@gmail.com>
2022-02-18 02:56:49 +0300

    Add get and post methods

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,34 @@
 const express = require('express');
-
 const app = express();
 const PORT = 4001;
+const cors = require('cors'); // Providing middleware
 
-app.get('/', (req, res) => res.send("Hello API"));
+app.use(cors()); // usage cors middleware
+app.use(express.json()); // Распознование объекта как JSON 
 
-app.listen(PORT, () => console.log(`Server is listening on PORT ${PORT}`))
+app.get('/', (req, res) => {
+    res.send(signup);
+})
+
+const signup = [];
+
+app.post('/login', async (req, res) => {
+    try {
+        let login = req.body.login;
+        let password = req.body.password;
+
+        let userLogin = {
+            login: login,
+            password: password
+        }
+
+        signup.push(userLogin);
+
+        return res.send(200).json("Hello, world!");
+
+    } catch (error) {
+        return res.status(400)
+    }
+})
+
+app.listen(4001, () => console.log("Server is listening on port", PORT));

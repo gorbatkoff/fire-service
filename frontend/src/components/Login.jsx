692ac34ba51995d4e27a577ae1c92d4cef397612
gorbatkoff <gorbatkovartyom@gmail.com>
2022-02-18 02:05:47 +0300

    Add posting data to API

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { message } from 'antd';
 
-export default function Login() {
+export default function Login({ logIn }) {
+
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+
+    const enterLogin = (e) => {
+        setLogin(e.target.value);
+        console.log(login);
+    }
+
+    const enterPassword = (e) => {
+        setPassword(e.target.value);
+        console.log(password);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            
+            logIn(login, password);
+
+        }
+
+        catch (err) {
+            message.error("Something gome wrong");
+        }
+
+    }
+
     return (
         <div className="login">
             <div className="login-container">
@@ -8,25 +38,28 @@ export default function Login() {
 
                 <div className="login-form">
                     <div className="username form">
+
                         <p>Логин</p>
-                        <input placeholder="Введите ваш логин"></input>
+
+                        <input placeholder="Введите ваш логин" onChange={enterLogin}></input>
+
                     </div>
 
                     <div className="password form">
                         <p>Пароль</p>
-                        <input placeholder="Введите ваш пароль"></input>
+
+                        <input placeholder="Введите ваш пароль" onChange={enterPassword}></input>
+
                     </div>
-
                 </div>
-
 
                 <div className="remember-me">
                     <input type="checkbox" />
                     <p>Remember Me</p>
                 </div>
 
-                <form action="" className="button-form">
-                    <button>
+                <form action="" className="button-form" onSubmit={handleSubmit}>
+                    <button >
                         Войти
                     </button>
                 </form>

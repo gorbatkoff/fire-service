e3bad98ca12af0e9bc0049f55f3d75f4a5746b83
gorbatkoff <gorbatkovartyom@gmail.com>
2022-02-21 16:03:12 +0300

    Add connecting with DataBase

--- backend/index.js
+++ backend/index.js
@@ -2,34 +2,75 @@ const express = require('express');
 const app = express();
 const PORT = 4001;
 const cors = require('cors'); // Providing middleware
-const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose');
+const db = 'mongodb+srv://qwerty:qwerty12@cluster0.bnpch.mongodb.net/fire-service-db?retryWrites=true&w=majority';
+
+
 
 app.use(cors()); // usage cors middleware
 app.use(express.json()); // Распознование объекта как JSON 
 
-app.get('/', (req, res) => {
-    res.send(signup);
-})
-
-const signup = [];
-
-app.post('/login', async (req, res) => {
+async function start() {
     try {
-        let login = req.body.login;
-        let password = req.body.password;
+        await mongoose.connect(db);
 
-        let userLogin = {
-            login: login,
-            password: password
-        }
+        console.log("Соединение с базой данных установлено");
 
-        signup.push(userLogin);
+        // await client.connect();
 
-        return res.send(200).json("Hello, world!");
+        // const users = client.db().collection('users');
 
-    } catch (error) {
-        return res.status(400)
+        // const user = await users.find();
+
+        // console.log(user);
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+app.listen(4001, () => console.log("Server is listening on port", PORT));
+
+start();
+
+app.get('/', async (req, res) => {
+
+    try {
+
+        await client.connect();
+        const users = await client.db().collection('users');
+
+        // const users = client.db().collection('users');
+        // console.log(db.users.find());
+        let b = users.find({ name: "Artem" });
+
+        console.log(b);
+        // res.send({users});
+
+        res.send(typeof b);
+
+    } catch (err) {
+        console.log(err);
     }
 })
 
-app.listen(4001, () => console.log("Server is listening on port", PORT));
+// const signup = [];
+
+// app.post('/login', async (req, res) => {
+//     try {
+//         let login = req.body.login;
+//         let password = req.body.password;
+
+//         let userLogin = {
+//             login: login,
+//             password: password
+//         }
+
+//         signup.push(userLogin);
+
+//         return res.send(200).json("Hello, world!");
+
+//     } catch (error) {
+//         return res.status(400)
+//     }
+// })
